f13f8795eebe0113abaf53f645c542e98a7e3fd2
Jakub Hozak <jakub.hozak@gmail.com>
2016-12-17 17:41:21 +0100

    * excersise 1

---
+++ excercise-1.rb
@@ -0,0 +1,9 @@
+# Vytisknete tabulku celych cisel od 1 do 20 a jejich
+# mocnin druhych, tretich ... 6 mocnin
+#
+
+(1..20).each do |i|
+  powers = (2..6).to_a.map { |exp| i**exp }
+  result = powers.join(', ')
+  puts "#{i}: #{result}"
+end
